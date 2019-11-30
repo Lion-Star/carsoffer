30f1244da1d9b62ee8f1c009d864a22533c2b9fc
Lion-Star <15007006754@163.com>
2019-11-30 08:53:21 +0800

    updata homepage

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import Axios from 'axios'
-import 'vant/lib/button/style';
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
