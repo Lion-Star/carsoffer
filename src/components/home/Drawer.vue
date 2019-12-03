672b90417d30672ef5cd04e570eb55cab6d4cb3c
Lion-Star <15007006754@163.com>
2019-12-03 20:00:28 +0800

    updateRem

---
+++ src/components/home/Drawer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="drawer"
+  ref="ele"
+  @touchstart="touchStart"
+  @touchmove="touchMove"
+  @touchend="touchEnd"
+  >
+    <li v-for="(item,index) in list" :key="index">{{item.title}}</li>
+  </div>
+</template>
+
+<script>
+export default  {
+  props:['list'],
+  data(){
+    return {
+      ind:0
+    }
+  },
+  created(){
+
+  },
+  watch:{
+    list:function(){
+      this.$nextTick(()=>{
+       
+        this.offsetTop =  (window.innerHeight - this.$refs.ele.offsetHeight)/2
+        console.log(window.innerHeight,this.$refs.ele.offsetHeight);
+      })
+    }
+  },
+  methods:{
+    touchStart(e){
+      let y = e.touches[0].pageY - this.offsetTop
+      let height=this.$refs.ele.children[0].offsetHeight
+      let index = Math.floor(y/height);         
+    },
+    touchMove(e){
+      let y = e.touches[0].pageY - this.offsetTop
+      let height=this.$refs.ele.children[0].offsetHeight
+      let index = Math.floor(y/height);
+      // 处理边界
+      // 处理边界
+      // index<1时 index=1 否则 就用index
+      // 当 this.list.length-1 >=0 即为本身 否则为null
+      index<0?index=0:index>this.list.length-1?index=this.list.length-1:null
+      this.$emit('skip', index);
+    },
+    touchEnd(e){
+      console.log('end.....',e);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.drawer{
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 50%;
+  transform: translate(48%,-50%);
+  right: 15px;
+  align-items: center;
+  text-align: center;
+  font-size: 16px;li{
+      list-style: none;
+      &.active{
+        color: #f56;
+      }
+    }
+}
+  
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,52 @@
 <template>
   <div class="home">
-    <div class="main" >
-      <div  class="box" ref="scrolllist">
-        <div class="select" v-for="(item,index) in list" :key="index">
-        <div class="top" :id="index">{{item.title}}</div>
-        <div class="bottom">
-          <li v-for="(val,i) in item.data" :key="i" @click="showPopup(val.MasterID)">
-            <img :src="val.CoverPhoto" />
-            <span>{{val.Name}}</span>
-          </li>
-        </div>
-      </div>
-    </div>
- 
-    
-      <!-- 弹窗组件 -->
-      <van-popup v-model="show" position="right" :style="{ height: '100%' }">
-        <div class="dialog" v-for="(item,index) in carlist" :key="index">
-          <div class="top">{{item.GroupName}}</div>
-          <div class="c-bottom" v-for="(val,i) in item.GroupList" :key="i">
-            <dl @click="detail(val)">
-              <dt>
-                <img :src="val.Picture" />
-              </dt>
-              <dd>
-                <span>{{val.AliasName}}</span>
-               
-                <b>{{val.DealerPrice}}</b>
-              </dd>
-            </dl>
+      <div class="box" ref="box">
+          <div class="select" v-for="(item,index) in list" :key="index" ref="select">
+          <div class="top" :id="index">{{item.title}}</div>
+          <div class="bottom">
+            <li v-for="(val,i) in item.data" :key="i" @click="showPopup(val.MasterID)">
+              <img :src="val.CoverPhoto" />
+              <span>{{val.Name}}</span>
+            </li>
           </div>
         </div>
-      </van-popup>
-    </div>
+      </div>
+    <!-- 弹窗组件 -->
+    <van-popup v-model="show" position="right" :style="{ height: '100%' }">
+      <div class="dialog" v-for="(item,index) in carlist" :key="index">
+        <div class="top">
+          {{item.GroupName}}
+        </div>
+        <div class="c-bottom" v-for="(val,i) in item.GroupList" :key="i">
+          <dl @click="detail(val)">
+            <dt>
+              <img :src="val.Picture" />
+            </dt>
+            <dd>
+              <span>{{val.AliasName}}</span>
+              <b>{{val.DealerPrice}}</b>
+            </dd>
+          </dl>
+        </div>
+      </div>
+    </van-popup>
     <!-- 点击跳转 -->
-    <div class="jump">
-     <ul>
-       <li v-for="(item,index) in list" :key="index" :class='{"active":ind==index}' @click='handclick(index)'>{{item.title}}</li>
-     </ul>
-    </div>
+    <Drawer :list="list" @skip="skip"></Drawer>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll'
-import {mapState,mapMutations,mapActions} from "vuex"
+import {mapState,mapMutations,mapActions} from 'vuex'
+import Drawer from "@/components/home/Drawer.vue"
 export default {
   name: "home",
-  components: {},
+  components: {
+    Drawer
+  },
   data() {
     return {
       show: false, //弹层显示隐藏
-      arrsort:[],
       left:null,
       right:null,
       ind:0,
@@ -64,25 +59,6 @@ export default {
       carlist:state=>state.home.carlist,
     })
   },
-  created() {
-    this.$nextTick(()=>{
-      this.scrollHeight();
-      this.left=new BScroll('.main',{
-        click:true,
-        probeType:2
-      })
-      this.left.on("scroll",(res)=>{
-      this.scrollH=Math.abs(res.y) ;
-        for(var j=0;j<this.list.length;j++){
-          var h1=this.list[j];
-          var h2=this.list[j+1]
-          if(h2 && (this.scrollH>=h1 && this.scrollH<h2)){
-            this.ind=j
-          }
-        }
-      })
-    })
-  },
   mounted(){
     this.getMasterBrandList()
   },
@@ -91,20 +67,6 @@ export default {
       getMasterBrandList: 'home/getMasterBrandList',
       getMakeListByMasterBrandId:'home/getMakeListByMasterBrandId'
     }),
-    scrollHeight(){
-      let children=this.$refs.scrolllist.children //获取到每个子元素
-      let height = 0
-      this.list.push(height)
-      for(var i=0;i<children.length;i++){
-          height+=children[i].offsetHeight;
-          this.list.push(height)
-      }
-    },
-    handclick(i){
-      this.ind=i;
-      var children=this.$refs.scrolllist.children;
-      this.left.scrollToElement(children[i],1000)
-    },
     //跳转详情
     detail(val){ 
       let item=JSON.stringify(val)
@@ -112,13 +74,14 @@ export default {
       this.$router.push("/details")
     },
     //点击弹窗
-    async showPopup(id) {
+    showPopup(id) {
       this.show = true;
       this.getMakeListByMasterBrandId(id)
-      // let res = await this.$http.get(
-      //   `https://baojia.chelun.com/v2-car-getMakeListByMasterBrandId.html?MasterID=${id}`
-      // );
-      // this.carlist = res.data.data;
+    },
+    //点击跳转
+    skip(index){
+      let boxel=this.$refs.box  
+      boxel.scrollTop = boxel.children[index].offsetTop;
     }
   }
 };
@@ -133,34 +96,34 @@ export default {
 .main{
   width: 100%;
   height: 100%;
-  overflow: hidden;
 }
 .box{
   width: 100%;
- 
+ height: 100%;
+ overflow-y: scroll
 }
- .top {
-    font-size: .32rem;
-    height: 30px;
-    line-height: 30px;
-    background: #eee;
-    color: #666;
-    padding-left: 12px;
-  }
+.top {
+  font-size: .32rem;
+  height: .60rem;
+  line-height: .60rem;
+  background: #eee;
+  color: #666;
+  padding-left: .24rem;
+}
 .select {
   width: 100%;
- 
   .bottom {
     width: 100%;
     li {
-      height: 40px;
+      height: .80rem;
       display: flex;
-      line-height: 40px;
-      margin: 15px;
+      line-height: .80rem;
+      margin: .30rem;
       border-bottom: 1px solid #ccc;
       img {
+        width: 20%;
         height: 100%;
-        padding-right: 20px;
+        padding-right: .40rem;
       }
     }
     li:last-child {
@@ -171,25 +134,24 @@ export default {
 .c-bottom {
   width: 100%;
   display: flex;
-  padding: 10px;
+  padding: .20rem;
   color: #333;
-   border-bottom: 1px solid #ccc;
+  border-bottom: .1rem solid #ccc;
   dl {
     display: flex;
     width: 100%;
     dd{
       height: 100%;
-      padding-left: 5px;
+      padding-left: .10rem;
       display: flex;
       flex-direction: column;
       justify-content: space-around;
       span{
-        font-size: 18px;
+        font-size: .36rem;
       }
       b{
-       
         color: #f00;
-        font-size: 14px;
+        font-size: .28rem;
         font-weight: 400;
       }
     }
@@ -197,30 +159,12 @@ export default {
       width: 40%;
       height: 100%;
       img{
-        width: 90px;
+        width:1.8rem;
       }
     }
   }
 }
-.jump{
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  top: 50%;
-  transform: translate(48%,-50%);
-  right: 15px;
-  align-items: center;
-  text-align: center;
-  font-size: 16px;
-  ul{
-    li{
-      list-style: none;
-      &.active{
-        color: #f56;
-      }
-    }
-  }
-}
+
 .van-popup--right {
   width: 75%;
 }
